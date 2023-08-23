af0a7e743a9c34eb609d8d6dd4f41ad572fbbd5e
Icesups <scottbusybody@gmail.com>
2023-08-23 21:50:20 +0530

    commit

---
+++ Assets/Scrips/LookAt.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+    static public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(target == null)
+        {
+            target=this.gameObject;
+            Debug.Log("Look at target target not specified. Defalting to GameObject");
+        }
+    }
+        
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            transform.LookAt(target.transform);
+        }
+    }
+}
